ec0081a0dfd81627ff1b6dd430f579803875a026
mitul72 <mituldhawan2@gmail.com>
2024-09-17 18:16:20 -0600

    Initial implementation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.glb$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/files",
+          outputPath: "static/files",
+          name: "[name].[hash].[ext]",
+        },
+      },
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,98 @@
-import Image from "next/image";
+"use client";
+import PokeTown from "@/components/models/pokemon-town";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { OrbitControls, useHelper } from "@react-three/drei";
+import PirateIsland from "@/components/models/pirate-island";
+import {
+  DirectionalLight,
+  DirectionalLightHelper,
+  HemisphereLight,
+} from "three";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [screenScale, setScreenScale] = useState([1, 1, 1]);
+  const [screenPosition, setScreenPosition] = useState([0, -6.5, -43]);
+  const [rotation, setRotation] = useState([0.1, 4.7, 0]);
+  const CameraTracker = () => {
+    const { camera } = useThree(); // Get access to the camera object
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+    useFrame(() => {
+      const { x, y, z } = camera.position;
+      console.log(
+        "Camera position: ",
+        x.toFixed(2),
+        y.toFixed(2),
+        z.toFixed(2)
+      );
+      console.log("Camera rotation: ", camera.rotation);
+      // setCameraPosition([x.toFixed(2), y.toFixed(2), z.toFixed(2)]); // Update the camera position state
+    });
+
+    return null;
+  };
+
+  // Run the window-related code inside useEffect to ensure it runs on the client side
+  useEffect(() => {
+    const adjustTownForScreen = () => {
+      let screenScale;
+      let screenPosition = [0, -6.5, -43];
+      let rotation = [0.1, 4.7, 0];
+
+      if (window.innerWidth < 768) {
+        screenScale = [0.9, 0.9, 0.9];
+      } else {
+        screenScale = [1, 1, 1];
+      }
+      return { screenScale, screenPosition, rotation };
+    };
+
+    const { screenScale, screenPosition, rotation } = adjustTownForScreen();
+    setScreenScale(screenScale);
+    setScreenPosition(screenPosition);
+    setRotation(rotation);
+
+    // Optional: Add an event listener to update on window resize
+    const handleResize = () => {
+      const { screenScale, screenPosition, rotation } = adjustTownForScreen();
+      setScreenScale(screenScale);
+      setScreenPosition(screenPosition);
+      setRotation(rotation);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{
+          position: [23.08, 30.52, 150.63], // Default camera position
+          rotation: [-0.199, 0.149, 0.03],
+          near: 0.1,
+          far: 1000,
+        }}
+      >
+        <Suspense fallback={null}>
+          <directionalLight
+            intensity={2.5}
+            scale={30}
+            position={[0, 240, 200]}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+          <hemisphereLight intensity={0.4} />
+          <ambientLight intensity={0.3} />
+          {/* <PokeTown
+            position={screenPosition}
+            scale={screenScale}
+            rotation={rotation}
+          /> */}
+          <PirateIsland />
+          {/* <CameraTracker /> */}
+        </Suspense>
+        <OrbitControls enableZoom={true} />
+      </Canvas>
+    </section>
   );
 }
